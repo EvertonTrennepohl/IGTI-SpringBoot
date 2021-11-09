22e89ca7630882ae4509e7bb8b8d0d7611317d62
EvertonTrennepohl <gauchotp@gmail.com>
2021-11-09 15:28:05 -0300

    Projeto criado - Desafio Módulo 4 - Vendas

---
+++ src/main/java/com/trennepohl/vendas/IgtiDesafioModulo04Application.java
@@ -0,0 +1,13 @@
+package com.trennepohl.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgtiDesafioModulo04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgtiDesafioModulo04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/trennepohl/vendas/IgtiDesafioModulo04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trennepohl.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgtiDesafioModulo04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
